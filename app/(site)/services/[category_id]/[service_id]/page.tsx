79e51e929c1f043b35ef92121af4fc2404417de7
Rahat393 <rahataminulislam37@gmail.com>
2024-08-23 17:42:53 +0600

    some fixing

--- app/(site)/services/[category_id]/[service_id]/page.tsx
+++ app/(site)/services/[category_id]/[service_id]/page.tsx
@@ -2,6 +2,7 @@ import PageHeader from "@/components/common/page-header";
 import backgroundImage from "@/images/about-bg.jpeg";
 import { ALL_SERVICES } from "@/shared/data";
 import { kebabCaseToNormalText } from "@/shared/function";
+import Image from "next/image";
 import ServicePropertyType from "./_components/service-property-type";
 
 export default function ServiceDetailsPage({
@@ -37,7 +38,21 @@ export default function ServiceDetailsPage({
         backgroundImage={backgroundImage}
         breadCrumbOptions={breadCrumbOptions}
       />
+
       <ServicePropertyType categoryId={category_id} serviceId={service_id} />
+      <div className="max-w-5xl mx-auto mt-20">
+        <h1 className="text-4xl font-bold text-center mx-auto mb-10 w-[65%]">
+          {currentService?.pageContent?.title}
+        </h1>
+        {currentService?.image && (
+          <Image
+            src={currentService?.image}
+            alt="serviceImage"
+            objectFit="cover"
+            className="w"
+          />
+        )}
+      </div>
     </>
   );
 }
