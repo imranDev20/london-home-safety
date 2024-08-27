03ca53ed181c7a7eded4043d59222089bbe40a05
Rahat393 <rahataminulislam37@gmail.com>
2024-08-27 13:50:23 +0600

    some bug fixing

--- app/(site)/_components/faq.tsx
+++ app/(site)/_components/faq.tsx
@@ -10,8 +10,13 @@ import Image from "next/image";
 
 export default function Faq() {
   return (
-    <div className="flex max-w-6xl mx-auto my-24">
-      <Image className="w-1/2" src={faqImage} alt="faqimage" loading="lazy" />
+    <div className="flex space-x-4 max-w-6xl mx-auto my-24 overflow-hidden">
+      <Image
+        className="w-1/2 h-[60%]"
+        src={faqImage}
+        alt="faqimage"
+        loading="lazy"
+      />
       <div>
         <h2 className="text-3xl font-bold">Frequently Asked Questions</h2>
         <p className=" text-body mt-6 mb-8 leading-loose">
@@ -22,11 +27,11 @@ export default function Faq() {
         <Accordion
           type="single"
           collapsible
-          className="w-full hover:no-underline "
+          className="w-full hover:no-underline"
         >
           {FAQ_HOME.map((item) => (
             <AccordionItem key={item.title} value={item.title}>
-              <AccordionTrigger className="text-lg font-semibold truncate hover:no-underline">
+              <AccordionTrigger className="text-lg font-semibold text-start  hover:no-underline">
                 {item.title}
               </AccordionTrigger>
               <AccordionContent className="text-lg  text-body">
