b9482541b8567e2e66beb893d1cd7de70d361910
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2024-08-29 19:35:04 +0600

    "Refactored ContactAddress and ContactDetails components: updated CSS classes, removed unused import, and adjusted layout and styling."

--- app/(site)/contact/_components/contact-address.tsx
+++ app/(site)/contact/_components/contact-address.tsx
@@ -1,15 +1,12 @@
 import { CONTACT } from "@/shared/data";
-import { Contact } from "@/types/contact";
 import ContactDetails from "./contact-details";
 
 export default function ContactAddress() {
-  
-
   return (
-    <div className="  bg-blue-50 py-20  ">
-      <div className="max-w-6xl mx-auto grid md:grid-cols-3 gap-">
+    <div className="bg-blue-50 py-12 md:py-20">
+      <div className="max-w-6xl px-5 justify-center items-center gap-10  mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {CONTACT.map((contact) => (
-         <ContactDetails key={contact.id} item={contact} />
+          <ContactDetails key={contact.id} item={contact} />
         ))}
       </div>
     </div>
--- app/(site)/contact/_components/contact-details.tsx
+++ app/(site)/contact/_components/contact-details.tsx
@@ -5,15 +5,19 @@ export const ContactDetails = ({ item }: { item: Contact }) => {
   return (
     <div
       key={item.id}
-      className="flex space-x-4 bg-white rounded-2xl p-6 shadow-lg w-[350px]"
+      className="flex space-x-4 bg-white rounded-2xl p-6 shadow-lg w-full md:w-[350px] mx-auto"
     >
-      <div> {item.icons}</div>
+      <div className="text-primary">{item.icons}</div>
       <div>
-        <h3 className="font-bold text-xl mb-4">{item.title}</h3>
-        <p className="text-body text-[1.10rem]   max-w-[260px] ">{item.info}</p>
+        <h3 className="font-bold text-lg md:text-xl mb-2 md:mb-4">
+          {item.title}
+        </h3>
+        <p className="text-body text-sm md:text-[1.10rem] max-w-full md:max-w-[260px]">
+          {item.info}
+        </p>
       </div>
     </div>
   );
 };
 
-export default ContactDetails
+export default ContactDetails;
