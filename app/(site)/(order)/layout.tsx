6d52d71e7caba6ffd42c6ef680e7bea4ed76385b
Spiderbyte IT <itspiderbyte@gmail.com>
2024-09-04 09:54:19 +0600

    Fix build issue related to stepper

--- app/(site)/(order)/layout.tsx
+++ app/(site)/(order)/layout.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import StepperController from "./_components/stepper-controller";
-import StepperLoader from "./_components/stepper-controller";
+import StepperLoader from "./_components/stepper-loader";
 
 export default function OrderLayout({
   children,
