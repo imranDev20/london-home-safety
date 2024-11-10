9df3499236450d11cd155636d42b66989d8204cc
Spiderbyte IT <itspiderbyte@gmail.com>
2024-11-11 00:52:47 +0600

    Fix db issue

--- prisma/migrations/20241110181817_time_slots_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Order" DROP CONSTRAINT "Order_timeSlotId_fkey";
-
--- AlterTable
-ALTER TABLE "Order" ALTER COLUMN "timeSlotId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_timeSlotId_fkey" FOREIGN KEY ("timeSlotId") REFERENCES "TimeSlot"("id") ON DELETE SET NULL ON UPDATE CASCADE;
